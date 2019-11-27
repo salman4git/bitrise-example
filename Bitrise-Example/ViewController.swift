946ecd1d5e591f9d13dd3fef6e4f60e02b4ec56b
Salman Siddiqui <salman.siddiqui@healthify.co>
2019-11-27 18:16:26 +0530

    Initial Commit

---
+++ Bitrise-Example/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Bitrise-Example
+//
+//  Created by Salman Siddiqui on 27/11/19.
+//  Copyright © 2019 Salman Siddiqui. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
